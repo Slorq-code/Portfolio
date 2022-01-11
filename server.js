d586f1ae97b6188010710397239d6aba1fdda251
andres <andresfra65@gmail.com>
2022-01-10 20:41:19 -0500

    prueba client -> build

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(cors());
 app.use("/", contactRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  //app.use(express.static("client/build"));
+  app.use(express.static("client"));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
